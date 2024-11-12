10ca1e10fd61513388054cc67b992d8683337dad
crashing <leo2872497@gmail.com>
2024-11-12 14:17:18 +0400

    innitial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int first = 10;
+        byte second = 9;
+        short third = 8;
+        long fourth = 7;
+        float fifth = 6.2F;
+        double sixth = 5.0;
+        System.out.println("Значение переменной first" + " с типом int " +  " равно " + first);
+        System.out.println("Значение переменной second" + " с типом byte " + " равно " + second);
+        System.out.println("Значение переменной third" + " с типом short " +  " равно " + third);
+        System.out.println("Значение переменной fourth" + " с типом long " +  " равно " + fourth);
+        System.out.println("Значение переменной fifth" + " с типом float " + " равно " + fifth);
+        System.out.println("Значение переменной sixth" + " с типом double " +  " равно " + sixth);
+        // zadacha 2 //
+        System.out.println("Задача 2");
+
+
+    }
+}
